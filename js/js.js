ecf7adf703914300a15a175bd121c816cad8dbed
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-04-13 18:27:49 -0400

    Disparo do jogador (helicoptero1)

--- js/js.js
+++ js/js.js
@@ -8,7 +8,7 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     //Principais variáveis do jogo
-        
+    var podeAtirar=true;
     var jogo = {}
     // variaveis atribuidas ao inimigo1 (helicoptero2)
     var velocidade=5;
@@ -92,6 +92,7 @@ function start() { // Inicio da função start()
         if (jogo.pressionou[TECLA.D]) {
             
             //Chama função Disparo	
+            disparo();
         }
     
     } // fim da função movejogador()
@@ -137,7 +138,39 @@ function start() { // Inicio da função start()
 
     }  // fim da função moveamigo()
 
-
+    function disparo() {
+	
+        if (podeAtirar==true) {
+            
+            podeAtirar=false;
+            
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX= parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro=topo+37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top",topoTiro);
+            $("#disparo").css("left",tiroX);
+            
+            var tempoDisparo=window.setInterval(executaDisparo, 30);
+            
+        } //Fecha podeAtirar
+     
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left",posicaoX+15); 
+    
+            if (posicaoX>900) {
+                    
+                window.clearInterval(tempoDisparo);
+                tempoDisparo=null;
+                $("#disparo").remove();
+                podeAtirar=true;
+            
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
+    
 
 
 } // Fim da função start
