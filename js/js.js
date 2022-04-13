6aa533e2f33ec85709f5d63078c81d6627ca07d1
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-04-13 17:01:55 -0400

    Limitar a movimentação do jogador (helicoptero) dentro da div fundoGame

--- js/js.js
+++ js/js.js
@@ -55,13 +55,27 @@ function start() { // Inicio da função start()
 	
         if (jogo.pressionou[TECLA.W]) {
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo-10);
+            if (topo >=10 ){
+                 $("#jogador").css("top",topo-10);
+            } else if (topo<=0) {
+		
+                $("#jogador").css("top",topo+10);
+            }
+            
         }
         
         if (jogo.pressionou[TECLA.S]) {
             
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo+10);	
+
+            if (topo <= 444) {
+                $("#jogador").css("top",topo+10);
+            }
+            /*
+            else if (topo >= 434) {	
+                $("#jogador").css("top",topo-10);
+                    
+            }*/	
         }
         
         if (jogo.pressionou[TECLA.D]) {
